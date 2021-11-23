95f50e03016a7997b62a1314f219e0a1fab119ce
linlifen <linlifen.lily@bytedance.com>
2021-11-23 09:02:28 +0800

    5th week homework

--- slidewindow/slidewindow.go
+++ slidewindow/slidewindow.go
@@ -12,12 +12,12 @@ type SlideWindow struct {
 	intervalMs int
 	count      int32
 	index      int
+	start      time.Time
 	mu         sync.Mutex
 	windows    []window
 }
 
 type window struct {
-	start int64
 	count int32
 }
 
@@ -27,7 +27,7 @@ func NewSlideWindwo(limit int32, intervalMs int) *SlideWindow {
 	}
 
 	num := (1000 / intervalMs)
-	return &SlideWindow{limit: limit, intervalMs: intervalMs, count: 0, index: 0, windows: make([]window, num)}
+	return &SlideWindow{limit: limit, intervalMs: intervalMs, count: 0, index: 0, start: time.Now(), windows: make([]window, num)}
 }
 
 func (slidewindow *SlideWindow) CanVisit() bool {
@@ -35,35 +35,46 @@ func (slidewindow *SlideWindow) CanVisit() bool {
 }
 
 func (slidewindow *SlideWindow) Visit() bool {
-	now := time.Now().UnixMilli()
+	now := time.Now()
 
 	slidewindow.mu.Lock()
 
-	index := slidewindow.index % cap(slidewindow.windows)
-
-	if slidewindow.count == 0 {
-		slidewindow.windows[index].start = now
-	} else {
-		if (int)(now-slidewindow.windows[index].start) >= slidewindow.intervalMs {
-			slidewindow.index = (int)(now-slidewindow.windows[index].start) / slidewindow.intervalMs
-			index = slidewindow.index % cap(slidewindow.windows)
-			if index == 0 {
-				slidewindow.count = slidewindow.count - slidewindow.windows[index].count
-				slidewindow.windows[index].count = 0
-			}
-			slidewindow.windows[index].start = now
+	diff := (int)(now.UnixMilli() - slidewindow.start.UnixMilli())
+	if diff < 1000 {
+		//一秒内，直接计算窗口索引
+		slidewindow.index = diff / slidewindow.intervalMs
+	} else if diff < 2000 {
+		//超过一秒，但是未超过两秒，窗口开始时间向前推移一秒
+		//同时，将过期的窗口的请求数清除
+		slidewindow.start = slidewindow.start.Add(time.Second)
+		slidewindow.index = (diff % 1000) / slidewindow.intervalMs
+		for i := 0; i <= slidewindow.index; i++ {
+			slidewindow.count -= slidewindow.windows[i].count
+			slidewindow.windows[i].count = 0
 		}
+	} else {
+		//超过2秒，则之前的所有请求都已经过期，全部清除，并且将窗口的开始时间向前推进相应的秒数
+		slidewindow.index = (diff % 1000) / slidewindow.intervalMs
+		seconds := (diff / 1000)
+		slidewindow.start = slidewindow.start.Add(time.Duration(seconds) * time.Second)
+		for i := range slidewindow.windows {
+			slidewindow.windows[i].count = 0
+		}
+		slidewindow.count = 0
 	}
 
+	slidewindow.windows[slidewindow.index].count++
 	slidewindow.count++
+
 	slidewindow.mu.Unlock()
 
 	if atomic.LoadInt32(&slidewindow.count) > slidewindow.limit {
 		atomic.AddInt32(&slidewindow.count, -1)
+		atomic.AddInt32(&slidewindow.windows[slidewindow.index].count, -1)
 		fmt.Printf("reject request\n")
 		return false
 	}
-	atomic.AddInt32(&slidewindow.windows[index].count, 1)
+
 	fmt.Printf("accept request\n")
 	return true
 }
